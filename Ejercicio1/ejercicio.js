0f05c26fed90f9bf581ae3b7a298ccb5c66ad653
GathoH3 <by.angh3l@gmail.com>
2024-05-09 18:16:41 -0400

    Ejercicio 1 resuelto

---
+++ Ejercicio1/ejercicio.js
@@ -0,0 +1,9 @@
+
+var nombre = prompt("Por favor, ingresa tu nombre:");
+var edad = prompt("Por favor, ingresa tu edad:");
+
+// Mostramos el nombre y la edad en la página HTML//
+
+
+document.write("Nombre: " + nombre + "<br>");
+document.write("Edad: " + edad + " años");
